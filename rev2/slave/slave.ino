6b9fffcf68e813aaa3d7486de72845e005ec4bab
Spinningplayer <Thijsvanmarle@gmail.com>
2017-05-19 10:18:52 +0200

    slave rev2 final

--- rev2/slave/slave.ino
+++ rev2/slave/slave.ino
@@ -22,40 +22,19 @@ void setup() {
 void loop() {
   TinyWireS_stop_check();
 
-  switch(RGBAX[5]) {
-    case 1:
-      
-    break;
-  }
 }
 
 void receiveEvent(uint8_t howMany) {
   int i = 0;
-  while(1 < TinyWireS.available()) {
+  while(TinyWireS.available() > 0) {
     RGBAX[i] = TinyWireS.receive();   
+    i++;
   }
   
   analogWrite(R, RGBAX[0]);
   analogWrite(G, RGBAX[1]);
   analogWrite(B, RGBAX[2]);
   analogWrite(A, RGBAX[3]);
-  
 }
 
-void wave(){
-  if(RGBAX[R] == 0 && RGBAX[G] == 0 && RGBAX[B] == 0) {
-    RGBAX[R]++;
-  } else if(RGBAX[R] == 255 && RGBAX[G] == 0 && RGBAX[B] == 0) {
-    RGBAX[G]++;
-  } else if(RGBAX[R] == 255 && RGBAX[G] == 255 && RGBAX[B] == 0) {
-    RGBAX[R]--;
-  } else if(RGBAX[R] == 0 && RGBAX[G] == 255 && RGBAX[B] == 0) {
-    RGBAX[B]++;
-  } else if(RGBAX[R] == 0 && RGBAX[G] == 255 && RGBAX[B] == 255) {
-    RGBAX[G]--;
-  } else if(RGBAX[R] == 0 && RGBAX[G] == 0 && RGBAX[B] == 255) {
-    RGBAX[R]++;
-  } else if(RGBAX[R]
-  
-}
 
