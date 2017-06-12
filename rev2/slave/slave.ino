8da900742cf2cfe841233d3b694808fe9e6c7cfd
Unknown <Thijsvanmarle@gmail.com>
2017-06-12 09:16:30 +0200

    timer prescaling

--- rev2/slave/slave.ino
+++ rev2/slave/slave.ino
@@ -9,10 +9,13 @@
 #define A 3
 
 int RGBAX[5];
+int counter = 0;
 
 void setup() {
-  TCCR1B = TCCR1B & B11111000 | B00000001; //this changes the PWM frequency to 32kHz
-  TCCR0B = TCCR0B & B11111000 | B00000001; //this changes the PWM frequency to 62.5kHz
+//  TCCR1B = TCCR1B & B11111000 | B00000001; //this changes the PWM frequency to 32kHz
+//  TCCR0B = TCCR0B & B11111000 | B00000001; //this changes the PWM frequency to 62.5kHz
+  TCCR1B = (0 << CS12), (0 << CS11), (1 << CS10);
+  TCCR0B = (0 << CS12), (0 << CS11), (1 << CS10);
   TinyWireS.begin(ADDRESS);
   TinyWireS.onReceive(receiveEvent);
   pinMode(R, OUTPUT);
@@ -24,7 +27,7 @@ void setup() {
 
 void loop() {
   TinyWireS_stop_check();
-
+//  modeStep();
 }
 
 void receiveEvent(uint8_t howMany) {
@@ -33,12 +36,23 @@ void receiveEvent(uint8_t howMany) {
     RGBAX[i] = TinyWireS.receive();   
     i++;
   }
-  
+//  modeStep();
+  setLed();
+}
+
+void setLed() {
   analogWrite(R, RGBAX[0]);
   analogWrite(G, RGBAX[1]);
   analogWrite(B, RGBAX[2]);
   analogWrite(A, RGBAX[3]);
 }
+//
+//void modeStep(){
+//   switch(RGBAX[4]){
+//    case 1:
+//      
+//  }
+//}
 
 
 
